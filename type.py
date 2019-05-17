4fd66f25b9861e13050fe454da59c0eb42656378
Joseph Shalabi <jshalabi@google.com>
2019-05-17 15:04:30 -0400

    Report cpm and accuracy at program close

--- type.py
+++ type.py
@@ -5,9 +5,6 @@ import random
 import time
 import signal
 
-num_chars = 128
-update_period = 0.5
-
 def CursesSetup():
     stdscr = curses.initscr()
     curses.noecho()
@@ -24,53 +21,55 @@ def SIGINT_Handler(signum, handler):
 signal.signal(signal.SIGINT, SIGINT_Handler)
 
 def get_next_char():
-	return random.randint(33, num_chars - 1)
+        num_chars = 126
+        return random.randint(33, num_chars - 1)
+
+num_chars = 0
+num_chars_right = 0
+def percent_correct():
+    percent = 0
+    if (num_chars != 0):
+        percent = (float(num_chars_right) / num_chars) * 100
+        percent = round(percent, 1)
+    return str(percent) + "%"
+
+start_time = time.time()
+def cpm():
+    time_elapsed = time.time() - start_time
+    cpm = 0
+    if time_elapsed != 0:
+        cpm = float(60 * num_chars_right) / time_elapsed
+        cpm = round(cpm, 1)
+    return str(cpm)
 
 def main():
-	# Number of character in queue
-	queue_size = 20 
-	queue = [get_next_char() for i in range(0, queue_size)]
-		
-	num_chars = 0
-	num_chars_right = 0
-	def percent_correct():
-		percent = 0 
-		if (num_chars != 0):
-			percent = (float(num_chars_right) / num_chars) * 100
-			percent = round(percent, 1)
-		return str(percent) + "%"
-	
-	start_time = time.time()
-	def cpm():
-		time_elapsed = time.time() - start_time
-		cpm = 0
-		if time_elapsed != 0:
-			cpm = float(60 * num_chars_right) / time_elapsed
-			cpm = round(cpm, 1)
-		return str(cpm)
-				
-	result = "+"
-	stdscr = CursesSetup()
-	while(True):
-		stdscr.clear()
-		chars = ''.join([str(chr(ch)) for ch in queue])
-		display = ''.join([result, "> ", chars])
-		stdscr.addstr(0, 0, display)
-		display = ''.join(["cpm: ", cpm(), " | ",
-				   "accuracy: ", percent_correct(), " | ",
-				   "ascii: ", str(queue[0])])
-		stdscr.addstr(1, 0, display, curses.COLOR_GREEN)
-		stdscr.refresh()
-		actual = stdscr.getch()
-		if queue[0] == actual:
-			result = "+"
-			num_chars_right += 1
-			queue.append(get_next_char())
-			queue.pop(0)
-		else:
-			result = "-"
-		num_chars += 1
+    global num_chars, num_chars_right, start_time
+    stdscr = CursesSetup()
+    result = "+"
+    # Initalize character queue
+    queue_size = 20
+    queue = [get_next_char() for i in range(0, queue_size)]
+    while(True):
+        stdscr.clear()
+        chars = ''.join([str(chr(ch)) for ch in queue])
+        display = ''.join([result, "> ", chars])
+        stdscr.addstr(0, 0, display)
+        display = ''.join(["cpm: ", cpm(), " | ",
+                   "accuracy: ", percent_correct(), " | ",
+                   "ascii: ", str(queue[0])])
+        stdscr.addstr(1, 0, display, curses.COLOR_GREEN)
+        stdscr.refresh()
+        actual = stdscr.getch()
+        if queue[0] == actual:
+            result = "+"
+            num_chars_right += 1
+            queue.append(get_next_char())
+            queue.pop(0)
+        else:
+            result = "-"
+        num_chars += 1
 try:
-	main()
+    main()
 finally:
-	CursesTeardown()
+    CursesTeardown()
+    print("cpm: " + cpm() + " @ " + percent_correct())
